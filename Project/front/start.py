13bf06841aa689d06c67dcb1ca17b2ec9135b57c
김익한 <kt0831@naver.com>
2020-10-26 17:11:50 +0900

    [+] add learn Settings display

--- Project/front/start.py
+++ Project/front/start.py
@@ -13,11 +13,58 @@ from tkinter import ttk
 UI_Path = './ui/NetworkSetting.ui'
 form_class = uic.loadUiType(UI_Path)[0]
 
+class AnotherFormLayout(QDialog):
+    NumGridRows = 3
+    NumButtons = 4
+
+    def __init__(self):
+        super().__init__()
+        self.createFormGroupBox()
+        
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+        
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.formDataPreprocessing)
+        mainLayout.addWidget(self.formNueralNetwork)
+        mainLayout.addWidget(self.formLearn)
+        mainLayout.addWidget(buttonBox)
+        self.setLayout(mainLayout)
+        
+        self.setWindowTitle("Nueral Network Settings")
+        
+    def createFormGroupBox(self):
+        # data preprocessing
+        self.formDataPreprocessing = QGroupBox("Data Preprocessing")
+        layout = QFormLayout()
+        self.lineTarget = QLineEdit()
+        layout.addRow(QLabel("target size:"), self.lineTarget)
+        layout.addRow(QLabel("class mode:"), QComboBox())
+        self.lineBatch = QLineEdit()
+        layout.addRow(QLabel("batch size:"), self.lineBatch)
+        self.lineRgb = QLineEdit()
+        layout.addRow(QLabel("rgb:"), self.lineRgb)
+        self.formDataPreprocessing.setLayout(layout)
+        # nn setting
+        self.formNueralNetwork = QGroupBox("Nueral Network")
+        layoutNN = QFormLayout()
+        layoutNN.addRow(QLabel("select NN:"), QComboBox())
+        self.formNueralNetwork.setLayout(layoutNN)
+        # Learn Settings
+        self.formLearn = QGroupBox("Learn Settings")
+        
+    def accept(self):
+        print('hi')
+        print(self.lineTarget.text(), self.lineBatch.text(), self.lineRgb.text())
+
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.learnSettingDisplay = AnotherFormLayout()
         self.pushButton.clicked.connect(self.pushButton1)
+        self.btnLearnSettings.clicked.connect(self.learnSettingsFn)
         
     # 더블클릭했을경우 실행되는 함수
     def OnDoubleClick(self, event):
@@ -76,6 +123,11 @@ class WindowClass(QMainWindow, form_class) :
 
         root.mainloop()
 
+    def learnSettingsFn(self, checked):
+        if self.learnSettingDisplay.isVisible():
+            self.learnSettingDisplay.hide()
+        else:
+            self.learnSettingDisplay.show()
 
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
