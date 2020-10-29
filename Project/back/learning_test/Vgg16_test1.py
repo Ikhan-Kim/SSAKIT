447d50e4d27fbee540b88164147b256438a95a22
김정원 <dnnls1@naver.com>
2020-10-29 16:14:35 +0900

    [+] add study material

--- Project/back/learning_test/Vgg16_test1.py
+++ Project/back/learning_test/Vgg16_test1.py
@@ -16,6 +16,7 @@ TRAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Define hyperparameter
 INPUT_SIZE = 32
 CHANNELS = 3
+# INPUT_SHAPE = (INPUT_SIZE, INPUT_SIZE, CHANNELS)
 NUM_CLASSES = 10
 NUM_TRAIN_IMGS = 50000
 NUM_TEST_IMGS = 10000
@@ -29,6 +30,10 @@ val_steps_per_epoch = NUM_TEST_IMGS // BATCH_SIZE
 
 X_train = X_train/255.0
 X_test = X_test/255.0
+
+# X_train = X_train.reshape(X_train.shape[0], *INPUT_SHAPE)
+# X_test = X_test.reshape(X_test.shape[0], *INPUT_SHAPE)
+
 
 # Load pre-trained model
 base_model = tf.keras.applications.VGG16(include_top=False, 
