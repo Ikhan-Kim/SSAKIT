8e1c286c1c20809a2f8fc153b48ca9606aee1c30
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-11-12 17:38:11 +0900

    [+] 코드정리 프론트
    Former-commit-id: 48fa4756fd569365fcaddfa3b87916f616e2ab8b

--- Project/front/start.py
+++ Project/front/start.py
@@ -74,13 +74,10 @@ class Worker(QRunnable):
 
     '''
 
-    def __init__(self, tbt, fig, canvas, settingsData, learn_train_path, learn_val_path, *args, **kwargs):
+    def __init__(self, settingsData, learn_train_path, learn_val_path, *args, **kwargs):
         super(Worker, self).__init__()
 
         # Store constructor arguments (re-used for processing)
-        self.textBox_terminal = tbt
-        self.fig = fig
-        self.canvas = canvas
         self.settingsData = settingsData
         self.learn_train_path = learn_train_path
         self.learn_val_path = learn_val_path
@@ -374,7 +371,7 @@ class WindowClass(QMainWindow, form_class):
         if self.learn_train_path:
             self.btnDisable()
             # Pass the function to execute
-            worker = Worker(self.textBox_terminal, self.fig, self.canvas, self.settingsData, self.learn_train_path, self.learn_val_path) # Any other args, kwargs are passed to the run function
+            worker = Worker(self.settingsData, self.learn_train_path, self.learn_val_path) # Any other args, kwargs are passed to the run function
             worker.signals.result.connect(self.print_output)
             worker.signals.finished.connect(self.thread_complete)
             worker.signals.progress.connect(self.progress_fn)
