8b603b354296c3f2e484348d9ca14bbc5c277317
김다영 <95da.yeong@gmail.com>
2020-11-15 19:45:51 +0900

    [+] Edit | PNG to JPG
    Former-commit-id: 5ee3c09f064d8db51fc7eae81f537bfdc615ec93

--- Project/front/start.py
+++ Project/front/start.py
@@ -485,13 +485,13 @@ class WindowClass(QMainWindow, form_class):
         # self.testModelSelectDisplay = TestModelSelect()
         pixmap = QtGui.QPixmap(self.mainImg)
         self.imgLabel.setPixmap(pixmap)
-        self.btnOpenDir.setIcon(QIcon('./assets/folder.png'))
+        self.btnOpenDir.setIcon(QIcon('./assets/folder.jpg'))
         self.btnOpenDir.setIconSize(QSize(23, 23)) 
-        self.btnDataLoad.setIcon(QIcon('./assets/img/imageUpload.png'))
+        self.btnDataLoad.setIcon(QIcon('./assets/img/imageUpload.jpg'))
         self.btnDataLoad.setIconSize(QSize(25, 25))        
-        self.megaphone_2.setIcon(QIcon('./assets/img/megaphone1.png'))
+        self.megaphone_2.setIcon(QIcon('./assets/img/megaphone1.jpg'))
         self.megaphone_2.setIconSize(QSize(30, 30))
-        self.f5Btn.setIcon(QIcon('./assets/img/sync.png'))
+        self.f5Btn.setIcon(QIcon('./assets/img/sync.jpg'))
         self.f5Btn.setIconSize(QSize(20, 20))
         
         # 버튼별 함수 실행
