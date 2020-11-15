98b354b31677b2ab51d421aab99ebda268511e22
김정원 <dnnls1@naver.com>
2020-11-15 18:24:09 +0900

    update networks
    Former-commit-id: 6c53ccd3312ccf698eca74e2cf4c5d8e96f3e995

--- Project/front/mymodules/ResNet152_test1.py
+++ Project/front/mymodules/ResNet152_test1.py
@@ -108,6 +108,10 @@ def Learn(augmentation, input_epochs, train_path, val_path, window):
     history = model.fit(train_generator, epochs=EPOCHS, steps_per_epoch=train_steps_per_epoch, validation_data = validation_generator, validation_steps=val_steps_per_epoch, verbose = 1,  callbacks=callbacks)
     window.textBox_terminal.append("Training Done!")
     val_loss = history.history['val_loss']
-    message = "Epoch: "+ str(np.argmin(val_loss)+1)+ " , Min val_loss: "+ str(round(np.min(val_loss), 4))
+    val_accuracy = history.history['val_accuracy']
+    
+    Max_val_accuracy = round(np.max(val_accuracy), 4))
+    Min_val_loss = round(np.min(val_loss), 4)
+    message = "Epoch: "+ str(np.argmin(val_loss)+1)+ " , Min val_loss: "+ str(Min_val_loss)
     window.textBox_terminal.append(message)
     plt.close()
