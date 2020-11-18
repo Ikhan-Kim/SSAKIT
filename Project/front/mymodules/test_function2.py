17f51e7f035089e2c5c537836bd6d3ba98f1667c
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-11-18 17:48:12 +0900

    [+] test multiTreading 취소 (confusion matrix 버그생김)
    Former-commit-id: 3a4a804895af16a8589793fc0616c02fb0286d38

--- Project/front/mymodules/test_function2.py
+++ Project/front/mymodules/test_function2.py
@@ -230,6 +230,7 @@ def test(model_name, window):
         for c in classes:
             classes2.append('_'.join(c.split('_')[1:]))
         # show confusion matrix
+        print(cm)
         window.confusionMatrixTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
         window.confusionMatrixTable.setColumnCount(len(cm))
         window.confusionMatrixTable.setHorizontalHeaderLabels(classes2)
@@ -254,21 +255,28 @@ def test(model_name, window):
         # show confusion matrix image
         window.confusionMatrixTable.cellClicked.connect(show_img)
 
-
         # show precision, recall, accuracy
         window.precisionTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
         window.precisionTable.setColumnCount(len(cm))
+        window.precisionTable.setRowCount(1)
         window.precisionTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        window.precisionTable.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
         window.recallTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
         window.recallTable.setRowCount(len(cm))
+        window.recallTable.setColumnCount(1)
+        window.recallTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         window.recallTable.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        tmpPrecision = []
-        tmpRecall = []
+        # tmpPrecision = []
+        # tmpRecall = []
         for i in range(len(cm)):
-            tmpPrecision.append(str(precision[i]) + "%")
-            tmpRecall.append(str(recall[i]) + "%")
-        window.precisionTable.setHorizontalHeaderLabels(tmpPrecision)
-        window.recallTable.setVerticalHeaderLabels(tmpRecall)
+            window.precisionTable.setItem(0, i, QTableWidgetItem(str(precision[i]) + "%"))
+            window.recallTable.setItem(i, 0, QTableWidgetItem(str(recall[i]) + "%"))
+        # window.precisionTable.setHorizontalHeaderLabels(tmpPrecision)
+        # window.recallTable.setVerticalHeaderLabels(tmpRecall)
+        window.precisionTable.verticalHeader().setVisible(False)
+        window.precisionTable.horizontalHeader().setVisible(False)
+        window.recallTable.verticalHeader().setVisible(False)
+        window.recallTable.horizontalHeader().setVisible(False)
 
         window.accuracyTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
         window.accuracyTable.setColumnCount(1)
